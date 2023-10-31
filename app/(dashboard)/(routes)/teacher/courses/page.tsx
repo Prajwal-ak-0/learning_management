1cdba6f6d0354aa236158fcaf8a3a474a46adac0
Prajwal <prajwalhassan7@gmail.com>
2023-10-31 08:51:17 +0530

    Added Data table

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,18 +1,30 @@
-"use client";
 
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { columns } from "./_components/Column";
+import { DataTable } from "./_components/DataTable";
 
-const CoursesPage = () => {
-    return (
-        <div className="p-6">
-            <Link href="/teacher/create">
-                <Button>
-                    New Courses
-                </Button>
-            </Link>
-        </div>
-    );
+async function getData(): Promise<any[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ];
 }
+
+const CoursesPage =async () => {
+    const data = await getData()
+
+  return (
+    <div className="p-6">
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
+};
 
 export default CoursesPage;
