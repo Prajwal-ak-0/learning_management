bf55ca0c25b375074de8b4742b33a4bdd2c4d30c
Prajwal <prajwalhassan7@gmail.com>
2023-11-06 23:35:18 +0530

    1.0

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,12 +3,8 @@ import { Navbar } from "./_components/Navbar";
 import { Sidebar } from "./_components/Sidebar";
 import { ToastProvider } from "@/components/providers/ToastProvider";
 
-const DashboardLayout = ({
-  children
-}: {
-  children: React.ReactNode;
-}) => {
-  return ( 
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
     <div className="h-full">
       <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
         <Navbar />
@@ -17,12 +13,12 @@ const DashboardLayout = ({
         <Sidebar />
       </div>
       <main className="md:pl-56 pt-[80px] h-full">
-        <ConfettiProvider/>
-        <ToastProvider/>
+        <ConfettiProvider />
+        <ToastProvider />
         {children}
       </main>
     </div>
-   );
-}
- 
+  );
+};
+
 export default DashboardLayout;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,29 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { ClerkProvider } from '@clerk/nextjs'
-import { ToastProvider } from '@/components/providers/ToastProvider'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ToastProvider } from "@/components/providers/ToastProvider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'V-demy',
-  description: 'Vdemy',
-}
+  title: "V-demy",
+  description: "Vdemy",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={inter.className}>
-        <ToastProvider/>
-        {children}
-      </body>
-    </html>
+      <html lang="en">
+        <body className={inter.className}>
+          <ToastProvider />
+          {children}
+        </body>
+      </html>
     </ClerkProvider>
-  )
+  );
 }
